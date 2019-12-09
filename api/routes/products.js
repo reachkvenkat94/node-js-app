b3cb274d4e3f492020d16592538dafc78cda3f3f
reachkvenkat94 <reachkvenkat94@github.com>
2019-12-09 13:44:30 -0800

    adding funtion to upload image

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/product');
 const mongoose = require('mongoose');
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
 
 
 // Handle incoming get Request
@@ -115,8 +117,8 @@ router.delete('/:productId',(req,res,next) => {
 });
 
 // Handle incoming POST Request
-router.post('/',(req,res,next) => {
-
+router.post('/',upload.single('productImage'),(req,res,next) => {
+    console.log(req.file);
     const product = new Product({
         _id: mongoose.Types.ObjectId(),
         name: req.body.name,
